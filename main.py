1d9bc804f319edb657d08ca9a485318ba86e6009
chiamalite-codingal <chiamalitecodingal@gmail.com>
2024-10-12 17:43:52 +0100

    polygon

---
+++ main.py
@@ -0,0 +1,10 @@
+import turtle
+turtle.Screen().bgcolor("purple")
+turtle.Screen().setup(300,300)
+
+pencil = turtle.Turtle()
+
+for loop in range(6):
+    pencil.forward(100)
+    pencil.right(60)
+turtle.done()
